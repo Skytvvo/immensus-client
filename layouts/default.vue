d8d96aa33d4d6dee8c6ba0b99fda8dace4918e35
ilyakazakou <ilyakazakou@gmail.com>
2024-03-12 00:55:52 +0300

    feat(Desktop Goods Card): - created the component - configured props & types

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@
   <div class="default--desktop">
     <DesktopNavAppBar/>
     <div class="default-content--desktop">
-      <DesktopPost :author="mockedAuthor" :post="mockedPost"/>
+      <!-- todo: move goods here -->
     </div>
   </div>
 </template>
@@ -38,6 +38,7 @@
     display: flex;
     flex-direction: column;
     gap: 16px;
+    height: 100vh;
   }
   .default-content--desktop{
     display: flex;
