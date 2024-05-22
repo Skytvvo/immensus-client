0aa1a894fdcad95bfcd4ddc0dad4d7b2776bbb85
ilyakazakou <ilyakazakou@gmail.com>
2024-05-23 00:46:06 +0300

    - styled orders page

--- pages/orders.vue
+++ pages/orders.vue
@@ -10,24 +10,33 @@ const COLORS_STATUS_MAP = {
   DELIVERED: 'greenyellow',
   CANCELED: 'red',
 }
+
+const STATUS_MAP = {
+  PROCESSING: "В обработке",
+  DELIVERING: 'В доставке',
+  DELIVERED: 'Доставлен',
+  CANCELED: 'Отменен',
+}
 </script>
 
 <template>
   <NuxtLayout name="orders">
-    <div class="flex flex-col gap-8">
-      <div v-for="order in orders" :key="order.id" class="flex flex-col gap-1">
-        <div class="text-lg">Адрес: {{ order.address }}</div>
-        <div class="bold text-lg">Статус: <span :style="`background: ${COLORS_STATUS_MAP[order.status]}`">{{ order.status }}</span></div>
+    <div class="flex flex-col gap-8 items-center p-20">
+      <div class="font-semibold text-2xl mb-4">Мои Заказы</div>
+      <div v-for="order in orders" :key="order.id" class="flex flex-col gap-4 px-20 py-10 shadow-2xl rounded-xl	" style="width: 640px">
+        <div><span class="font-semibold">Адрес:</span> {{ order.address }}</div>
+        <div class="bold text-lg"><span class="font-semibold">Статус:</span> <span :style="`background: ${COLORS_STATUS_MAP[order.status]}`">{{ STATUS_MAP[order.status] }}</span></div>
         <div v-for="product in order.products" class="flex flex-row gap-2">
           <NuxtLink :to="`/item/${product.id}`">
             <img class="w-20 h-20" :src="product.picture" :alt="product.name" >
           </NuxtLink>
           <div class="flex flex-col gap-1">
-            <div>Название: {{ product.name }}</div>
-            <div>Цена: {{ product.price }}</div>
+            <div><span class="font-semibold">Название:</span> {{ product.name }}</div>
+            <div><span class="font-semibold">Цена:</span> {{ product.price }}</div>
+            <div class="flex"><span class="font-semibold">Описание:</span> {{ product.description }}</div>
           </div>
-          <div class="flex">Описание: {{ product.description }}</div>
         </div>
+        <div class="mt-4 font-semibold">Всего: {{order.products.reduce((acc, curr) => acc + curr.price, 0)}} Руб</div>
       </div>
     </div>
   </NuxtLayout>
